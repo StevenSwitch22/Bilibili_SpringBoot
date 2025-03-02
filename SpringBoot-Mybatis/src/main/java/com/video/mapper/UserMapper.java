a30ba5547108872df6a25d345f2142b64980bc79
StevenSwitch22 <dingswitcha@gmail.com>
2025-03-02 18:03:07 +0800

    开始实践Mybatis

---
+++ SpringBoot-Mybatis/src/main/java/com/video/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.video.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 类名：
+ *
+ * @author Acer
+ * 日期：2025/3/2
+ * 时间：17:55
+ */
+@Mapper  // 运行时，会自动生成该接口的实现类对象（），并将该对象交给IOC容器管理
+public interface UserMapper {
+}
