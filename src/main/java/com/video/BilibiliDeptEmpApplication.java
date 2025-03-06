959aef6c7d1f7a64f5228c4f564cf6fa999f2e65
StevenSwitch22 <dingswitcha@gmail.com>
2025-03-06 17:58:01 +0800

    无论在哪都用这个Case

---
+++ src/main/java/com/video/BilibiliDeptEmpApplication.java
@@ -0,0 +1,13 @@
+package com.video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilibiliDeptEmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilibiliDeptEmpApplication.class, args);
+    }
+
+}
